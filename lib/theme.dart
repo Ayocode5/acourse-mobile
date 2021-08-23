70cddd20d4e0015451ea2f148753e06a30672ba2
JonathanTriC <jonathantric@gmail.com>
2021-08-23 22:15:30 +0700

    add theme & add assets to pubspec

---
+++ lib/theme.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+double defaultMargin = 32;
+
+Color primaryBackgroundColor = Color(0xffF5F5F5);
+Color whiteColor = Color(0xffFFFFFF);
+Color blackColor = Color(0xff2E2E2E);
+Color blueColor = Color(0xff4279EE);
+Color lightBlueColor = Color(0xffECF2FD);
+Color grayColor = Color(0xffDADADA);
+
+TextStyle blackTextStyle = GoogleFonts.rubik(
+  color: blackColor,
+);
+
+TextStyle blueTextStyle = GoogleFonts.rubik(
+  color: blueColor,
+);
+
+TextStyle grayTextStyle = GoogleFonts.rubik(
+  color: grayColor,
+);
